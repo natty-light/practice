91885f0c341ac172802832fbdba46ff783ac69ca
natty-light <75773163+natty-light@users.noreply.github.com>
2023-01-23 08:26:27 -0500

    Started implementing 03

--- 03/solution.go
+++ 03/solution.go
@@ -1,9 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"strings"
+)
 
+func main() {
+	var in string = "abcabcbb"
+	fmt.Println(lengthOfLongestSubstring(in))
 }
 
 func lengthOfLongestSubstring(s string) int {
-
+	var best, current int = 0, 0
+	var dict = make(map[string]bool)
+	for _, char := range strings.Split(s, "") {
+		if _, exists := dict[char]; exists {
+			if best > current {
+				best = current
+			}
+			current = 1
+			dict = make(map[string]bool)
+			dict[char] = true
+		} else {
+			current += 1
+			dict[char] = true
+		}
+	}
+	if best > current {
+		return best
+	}
+	return current
 }
